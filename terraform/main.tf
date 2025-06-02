e610f4c3417ddd3f9ba02c88a6dddb8514885276
bryzle <brandonlum17@gmail.com>
2025-06-01 23:45:35 -0400

    Refactor AWS instance configuration in Terraform; add key_name and connection block for SSH access

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "devops_server" {
   ami           = "ami-05c13eab67c5d8861" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-
+  key_name      = "test"
   tags = {
     Name = "DevOpsServer"
   }
@@ -19,6 +19,13 @@ resource "aws_instance" "devops_server" {
       "sudo usermod -a -G docker ec2-user",
       "docker run -d -p 8080:8080 ${var.docker_image}"
     ]
+  }
+
+    connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
   }
 }
 
