db22a2efd47aa7d17f77a66cddbda37fcd56caf7
BRANDON LUM <brandonlum17@gmail.com>
2025-06-03 17:54:38 -0400

    Update security group to allow SSH access from any IP address

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "devops_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_PUBLIC_IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   # Egress (outbound)
   egress {
